9ad0834af92e0c6e491620bbc5396175a3e1af45
734666093@qq.com <734666093@qq.com>
2021-04-14 09:23:13 +0800

    new record

---
+++ 变量/用户交互.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Time : 2021/4/13 15:53
+# @Author : Pactera
+# @Email : wayne_lau@aliyun.com
+# @File : 用户交互.py
+# @time:2021/04/13
+# @Project : learnPython
+
+# input print
